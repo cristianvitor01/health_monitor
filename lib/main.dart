f226418471fa91d15fe238e192b37e9875f6aa05
cristianvitor01 <cristianvitoralves@gmail.com>
2024-12-27 18:54:30 -0300

    Adicionado PopupMenuButtonWidget ao Dashboard

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import './widgets/search_box.dart';
 import './widgets/appointment_card.dart';
 import './widgets/medication_card.dart';
 import './widgets/bottom_nav_bar.dart';
+import './widgets/popup_menu_buttom.dart';
 
 void main() {
   runApp(const MainApp());
@@ -29,75 +30,85 @@ class DashboardPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: UIColor.backgroundColor,
-      body: ListView(
-        // Para permitir rolagem
-        padding: const EdgeInsets.all(16.0),
+      body: Stack(
         children: [
-          const DashboardHeader(), // Cabeçalho no topo
-          const SizedBox(height: 16.0), // Espaço entre widgets
-          Row(
+          ListView(
+            // Para permitir rolagem
+            padding: const EdgeInsets.all(16.0),
             children: [
-              const Expanded(
-                child: SearchBox(hintText: 'Search your records'), // search_box
-              ),
-              const SizedBox(width: 10.0),
-              Stack(
+              const DashboardHeader(), // Cabeçalho no topo
+              const SizedBox(height: 16.0), // Espaço entre widgets
+              Row(
                 children: [
-                  Container( // Botão de notificação
-                    decoration: BoxDecoration(
-                        color: UIColor.iconsColor,
-                        borderRadius: BorderRadius.circular(14.0)),
-                    child: IconButton(
-                      onPressed: () {},
-                      icon:
-                          const Icon(Icons.notifications, color: Colors.white),
-                    ),
+                  const Expanded(
+                    child: SearchBox(hintText: 'Search your records'), // search_box
                   ),
-                  Positioned( // Bolinha vermelha da notificação
-                    right: 4,
-                    top: 4,
-                    child: Container(
-                      width: 8.0,
-                      height: 8.0,
-                      decoration: const BoxDecoration(
-                        color: Colors.red,
-                        shape: BoxShape.circle,
+                  const SizedBox(width: 10.0),
+                  Stack(
+                    children: [
+                      Container( // Botão de notificação
+                        decoration: BoxDecoration(
+                            color: UIColor.iconsColor,
+                            borderRadius: BorderRadius.circular(14.0)),
+                        child: IconButton(
+                          onPressed: () {},
+                          icon:
+                              const Icon(Icons.notifications, color: Colors.white),
+                        ),
                       ),
-                    ),
+                      Positioned( // Bolinha vermelha da notificação
+                        right: 4,
+                        top: 4,
+                        child: Container(
+                          width: 8.0,
+                          height: 8.0,
+                          decoration: const BoxDecoration(
+                            color: Colors.red,
+                            shape: BoxShape.circle,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
+              const SizedBox(height: 24.0),
+              const Text(
+                '  Your next Appointment',
+                style: TextStyle(
+                    fontFamily: 'Poppins', fontSize: 24.0, color: UIColor.textMain),
+              ),
+              const SizedBox(height: 16.0),
+              const AppointmentCard( // appointment_card
+                doctorName: 'Dr. Schmitz',
+                doctorSpecialty: 'Neurologist',
+                email: 'schmitz@aol.de',
+                phone: '+49 989 232',
+                date: '25/12/2024',
+                time: '16:30',
+                address: 'Winklergasse 45, 10117 Berlin',
+              ),
+              const SizedBox(height: 24.0),
+              const Text(
+                '   Your Medications Today',
+                style: TextStyle(
+                    fontFamily: 'Poppins', fontSize: 24.0, color: UIColor.textMain),
+              ),
+              const SizedBox(height: 16.0),
+              const MedicationCard( // medication_card
+                medicationName: 'Aspirin',
+                medicationDescription: 'ASS, 500mg BAYER, coated pills',
+                daily: '2x Daily',
+                time1: '06:00',
+                time2: '22:00',
+              ),
+              const SizedBox(height: 24.0),
             ],
           ),
-          const SizedBox(height: 24.0),
-          const Text(
-            '  Your next Appointment',
-            style: TextStyle(
-                fontFamily: 'Poppins', fontSize: 24.0, color: UIColor.textMain),
-          ),
-          const SizedBox(height: 16.0),
-          const AppointmentCard( // appointment_card
-            doctorName: 'Dr. Schmitz',
-            doctorSpecialty: 'Neurologist',
-            email: 'schmitz@aol.de',
-            phone: '+49 989 232',
-            date: '25/12/2024',
-            time: '16:30',
-            address: 'Winklergasse 45, 10117 Berlin',
-          ),
-          const SizedBox(height: 24.0),
-          const Text(
-            '   Your Medications Today',
-            style: TextStyle(
-                fontFamily: 'Poppins', fontSize: 24.0, color: UIColor.textMain),
-          ),
-          const SizedBox(height: 16.0),
-          const MedicationCard( // medication_card
-            medicationName: 'Aspirin',
-            medicationDescription: 'ASS, 500mg BAYER, coated pills',
-            daily: '2x Daily',
-            time1: '06:00',
-            time2: '22:00',
+          const Positioned(
+            bottom: 20.0, 
+            right: 22.0,
+            child: const PopupMenuButtonWidget(), // Adicionando o PopupMenuButtonWidget
           ),
         ],
       ),
